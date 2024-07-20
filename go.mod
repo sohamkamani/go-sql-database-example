45955301a71de0435201ab41af2c76ab4ff9317a
sohamkamani <sohamkamani@gmail.com>
2024-07-20 11:38:27 +0530

    update dependencies and code for new version

--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
 module github.com/sohamkamani/go-sql-database-example
 
-go 1.16
+go 1.20
 
 require (
-	github.com/jackc/pgx v3.6.2+incompatible // indirect
-	github.com/jackc/pgx/v4 v4.13.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
-	// we have to import the driver, but don't use it in our code
-	// so we use the `_` symbol
-	_ "github.com/jackc/pgx/v4/stdlib"
+	// Importing pgx v5 for PostgreSQL database operations. The pgx package is used
+	// directly for database connection and operations, replacing the standard database/sql package.
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type Bird struct {
@@ -20,12 +21,11 @@ type Bird struct {
 
 func main() {
 	// The `sql.Open` function opens a new `*sql.DB` instance. We specify the driver name
-	// and the URI for our database. Here, we're using a Postgres URI
-	db, err := sql.Open("pgx", "postgresql://localhost:5432/bird_encyclopedia")
+	// and the URI for our database. Here, we're using a Postgres URI from an environment variable
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("could not connect to database: %v", err)
 	}
-
 	// Maximum Idle Connections
 	db.SetMaxIdleConns(5)
 	// Maximum Open Connections
@@ -36,28 +36,18 @@ func main() {
 	db.SetConnMaxLifetime(30 * time.Second)
 
 	// To verify the connection to our database instance, we can call the `Ping`
-	// method. If no error is returned, we can assume a successful connection
-	if err := db.Ping(); err != nil {
+	// method with a context. If no error is returned, we can assume a successful connection
+	if err := db.PingContext(context.Background()); err != nil {
 		log.Fatalf("unable to reach database: %v", err)
 	}
 	fmt.Println("database is reachable")
 
-	// ctx := context.Background()
-	// conn, err := db.Conn(ctx)
-	// if err != nil {
-	// 	log.Fatalf("could not get connection: %v", err)
-	// }
+	queryCancellation(db)
+}
 
+func queryRow(db *sql.DB) {
 	// `QueryRow` always returns a single row from the database
-	birdName := "eagle"
-	// For Postgres, parameters are specified using the "$" symbol, along with the index of
-	// the param. Variables should be added as arguments in the same order
-	// The sql library takes care of converting types from Go to SQL based on the driver
-	row := db.QueryRow("SELECT bird, description FROM birds WHERE bird = $1 LIMIT $2", birdName, 1)
-
-	// the code to scan the obtained row is the same as before
-	//...
-
+	row := db.QueryRow("SELECT bird, description FROM birds LIMIT 1")
 	// Create a new `Bird` instance to hold our query results
 	bird := Bird{}
 	// the retrieved columns in our row are written to the provided addresses
@@ -67,7 +57,9 @@ func main() {
 		log.Fatalf("could not scan row: %v", err)
 	}
 	fmt.Printf("found bird: %+v\n", bird)
+}
 
+func queryRows(db *sql.DB) {
 	rows, err := db.Query("SELECT bird, description FROM birds limit 10")
 	if err != nil {
 		log.Fatalf("could not execute query: %v", err)
@@ -88,14 +80,10 @@ func main() {
 		birds = append(birds, bird)
 	}
 	// print the length, and all the birds
-	fmt.Printf("found %d birds: %+v\n", len(birds), birds)
+	fmt.Printf("found %d birds: %+v", len(birds), birds)
+}
 
-	// this is just to remove the inserted row from the previous run
-	_, err = db.Exec("DELETE FROM birds WHERE bird=$1", "rooster")
-	if err != nil {
-		log.Fatalf("could not delete row: %v", err)
-	}
-
+func insertRow(db *sql.DB) {
 	// sample data that we want to insert
 	newBird := Bird{
 		Species:     "rooster",
@@ -118,16 +106,37 @@ func main() {
 	}
 	// we can log how many rows were inserted
 	fmt.Println("inserted", rowsAffected, "rows")
+}
 
+func executePreparedStatement(db *sql.DB) {
+	// 1. Prepare the statement
+	stmt, err := db.Prepare("SELECT bird, description FROM birds WHERE bird = $1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close() // Important to close prepared statements
+
+	// 2. Execute the statement with a parameter
+	var bird Bird
+	err = stmt.QueryRow("eagle").Scan(&bird.Species, &bird.Description)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("result: %+v", bird)
+}
+
+func queryCancellation(db *sql.DB) {
 	// create a parent context
 	ctx := context.Background()
 	// create a context from the parent context with a 300ms timeout
 	ctx, _ = context.WithTimeout(ctx, 300*time.Millisecond)
 	// The context variable is passed to the `QueryContext` method as
 	// the first argument
-	_, err = db.QueryContext(ctx, "SELECT * from pg_sleep(1)")
+	// the pg_sleep method is a function in Postgres that will halt for
+	// the provided number of seconds. We can use this to simulate a
+	// slow query
+	_, err := db.QueryContext(ctx, "SELECT * from pg_sleep(1)")
 	if err != nil {
 		log.Fatalf("could not execute query: %v", err)
 	}
-
 }
